5e6e34fac3b2277dc494f07c84fb62e3d207a2fc
Sankarsan Kampa <snkrsn.kampa@gmail.com>
2017-02-26 00:08:38 +0530

    Code improvements - linux installer

--- BastionInstaller.sh
+++ BastionInstaller.sh
@@ -154,14 +154,15 @@ echo -e "${CYAN}[Bastion]:${NC} System files successfully installed."
 echo
 
 echo -e "${CYAN}[Bastion]:${NC} Finalizing..."
-cd settings && cp config_example.json config.json && cp credentials_example.json credentials.json && echo -e "${CYAN}[Bastion]:${NC} Done." || (echo -e "${CYAN}[Bastion]: ${RED}[ERROR] Bastion BOT settings directory was not found.${NC} Run the installer again or ask for help in Bastion BOT Official Server (https://discord.gg/fzx8fkt)." && exit 1)
-echo
-echo -e "${CYAN}[Bastion]:${NC} Do you want to setup credentials now?"
-echo -en "${GREEN}[User]:${NC} "
-read -n 1 -r
-echo
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
+(
+  (cd settings && cp config_example.json config.json && cp credentials_example.json credentials.json && echo -e "${CYAN}[Bastion]:${NC} Done.") || (echo -e "${CYAN}[Bastion]: ${RED}[ERROR] Bastion BOT settings directory was not found.${NC} Run the installer again or ask for help in Bastion BOT Official Server (https://discord.gg/fzx8fkt)." && exit 1)
+  echo
+  echo -e "${CYAN}[Bastion]:${NC} Do you want to setup credentials now?"
+  echo -en "${GREEN}[User]:${NC} "
+  read -n 1 -r
+  echo
+  if [[ $REPLY =~ ^[Yy]$ ]]
+  then
     echo -e "${CYAN}[Bastion]:${NC} Please enter the BOT ID"
     echo -en "${GREEN}[User]:${NC} "
     read -r botId
@@ -171,26 +172,26 @@ then
     echo -e "${CYAN}[Bastion]:${NC} Please enter the Owner ID"
     echo -en "${GREEN}[User]:${NC} "
     read -r ownerId
-fi
-{
-  echo "{"
-  echo "  \"botId\": \"$botId\","
-  echo "  \"token\": \"$token\","
-  echo "  \"ownerId\": ["
-  echo "    \"$ownerId\""
-  echo "  ]"
-  echo "}"
-} > credentials.json
-echo
-echo -e "${CYAN}[Bastion]:${NC} Do you want to configure BOT now?"
-echo -en "${GREEN}[User]:${NC} "
-read -n 1 -r
-echo
-prefix="bas?"
-status="online"
-game="with servers"
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
+  fi
+  {
+    echo "{"
+    echo "  \"botId\": \"$botId\","
+    echo "  \"token\": \"$token\","
+    echo "  \"ownerId\": ["
+    echo "    \"$ownerId\""
+    echo "  ]"
+    echo "}"
+  } > credentials.json
+  echo
+  echo -e "${CYAN}[Bastion]:${NC} Do you want to configure BOT now?"
+  echo -en "${GREEN}[User]:${NC} "
+  read -n 1 -r
+  echo
+  prefix="bas?"
+  status="online"
+  game="with servers"
+  if [[ $REPLY =~ ^[Yy]$ ]]
+  then
     echo -e "${CYAN}[Bastion]:${NC} What should be the commands' prefix? [Default: bas?]"
     echo -en "${GREEN}[User]:${NC} "
     read -r prefix
@@ -201,15 +202,15 @@ then
     echo -e "${CYAN}[Bastion]:${NC} What should be the BOT's game? [Default: with servers]"
     echo -en "${GREEN}[User]:${NC} "
     read -r game
-fi
-{
-  echo "{"
-  echo "  \"prefix\": \"$prefix\","
-  echo "  \"status\": \"$status\","
-  echo "  \"game\": \"$game\""
-  echo "}"
-} > config.json
-cd ..
+  fi
+  {
+    echo "{"
+    echo "  \"prefix\": \"$prefix\","
+    echo "  \"status\": \"$status\","
+    echo "  \"game\": \"$game\""
+    echo "}"
+  } > config.json
+)
 echo
 
 echo -e "${CYAN}[Bastion]:${NC} System Initialized. O7"
