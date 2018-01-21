fbf9b9bfd80588b6d74472e859ca118e5d7f73e4
Sankarsan Kampa <me@sankarsankampa.com>
2018-01-21 09:46:30 +0000

    macOS: Updated default credentials.json for v6.6

--- BastionInstaller_macOS.sh
+++ BastionInstaller_macOS.sh
@@ -103,6 +103,12 @@ echo -e "${CYAN}[Bastion]:${NC} Finalizing..."
     echo -e "${CYAN}[Bastion]:${NC} Please enter your Google API Key"
     echo -en "${GREEN}[User]:${NC} "
     read -r gAPIkey
+    echo -e "${CYAN}[Bastion]:${NC} Please enter your GitHub Personal Access Token"
+    echo -en "${GREEN}[User]:${NC} "
+    read -r githubAccessToken
+    echo -e "${CYAN}[Bastion]:${NC} Please enter your Patreon Creator Access Token"
+    echo -en "${GREEN}[User]:${NC} "
+    read -r patreonCreatorAccessToken
     echo -e "${CYAN}[Bastion]:${NC} Please enter your Twitch API Client ID"
     echo -en "${GREEN}[User]:${NC} "
     read -r twitchClientID
@@ -158,7 +164,16 @@ echo -e "${CYAN}[Bastion]:${NC} Finalizing..."
     echo "  \"HiRezAuthKey\": \"$HiRezAuthKey\","
     echo "  \"theMovieDBApiKey\": \"$theMovieDBApiKey\","
     echo "  \"musixmatchAPIKey\": \"$musixmatchAPIKey\","
-    echo "  \"cleverbotAPIkey\": \"$chatAPIkey\""
+    echo "  \"cleverbotAPIkey\": \"$chatAPIkey\","
+    echo "  \"github\": {"
+    echo "    \"accessToken\": \"$githubAccessToken\""
+    echo "  },"
+    echo "  \"patreon\": {"
+    echo "    \"clientID\": \"\","
+    echo "    \"clientSecret\": \"\","
+    echo "    \"creatorAccessToken\": \"$patreonCreatorAccessToken\","
+    echo "    \"creatorRefreshToken\": \"\""
+    echo "  }"
     echo "}"
   } > credentials.json
   echo
