efc269f9663b1dd9f2ef98738486a1fb2ed14c42
Sankarsan Kampa <me@sankarsankampa.com>
2018-02-04 08:15:12 +0000

    Install from stable branch instead of master

--- BastionInstaller.cmd
+++ BastionInstaller.cmd
@@ -54,7 +54,7 @@ node --version >nul 2>&1 && ECHO [Bastion]: Node is already installed. Looks goo
 ECHO.
 
 ECHO [Bastion]: Installing system files...
-git clone -b master -q --depth 1 https://github.com/TheBastionBot/Bastion.git >nul 2>&1 || (ECHO [Bastion]: Unable to download Bastion System files. Check your internet connection. && GOTO :EXIT)
+git clone -b stable -q --depth 1 https://github.com/TheBastionBot/Bastion.git >nul 2>&1 || (ECHO [Bastion]: Unable to download Bastion System files. Check your internet connection. && GOTO :EXIT)
 
 ::TODO: Find a way to show a message when npm install fails.
 CD Bastion && CALL npm i --only=production --no-package-lock >nul 2>&1
