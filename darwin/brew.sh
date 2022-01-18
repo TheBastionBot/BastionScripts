715e515f37d9e6754bf8ae9d62394a586113deb0
TRACTION <19631364+iamtraction@users.noreply.github.com>
2022-01-18 17:22:14 +0530

    darwin(brew): update node version

--- darwin/brew.sh
+++ darwin/brew.sh
@@ -137,11 +137,11 @@ function install::nodejs() {
   print::bastion "Installing Node.js..."
 
   if ! hash node &>/dev/null; then
-    brew install "node@14" || \
+    brew install "node@16" || \
       print::error "Unable to download and install Node.js."
   fi
 
-  if [ "$(node --version | cut -d'v' -f 2 | cut -d'.' -f 1)" -ne 14 ]; then
+  if [ "$(node --version | cut -d'v' -f 2 | cut -d'.' -f 1)" -ne 16 ]; then
     print::error "Please upgrade Node.js LTS before running the installer again."
   fi
 
