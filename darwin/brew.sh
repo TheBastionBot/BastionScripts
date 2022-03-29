51a19e9841108b68e078d5a53a470ea2fcb4611e
TRACTION <19631364+iamtraction@users.noreply.github.com>
2022-03-29 17:10:23 +0530

    darwin(brew): update brew installation method

--- darwin/brew.sh
+++ darwin/brew.sh
@@ -120,7 +120,7 @@ function install::packages() {
   sudo xcode-select --install || \
     print::warning "Unable to download & install XCode Command Line Tools. Maybe it's already installed?"
 
-  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" || \
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" || \
     print::error "Unable to download & install Homebrew."
 
   brew update -qq || print::error "Unable to update Homebrew."
