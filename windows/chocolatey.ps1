4250650a376e382ad7b962a329d78fbfc8e32dbf
TRACTION <19631364+iamtraction@users.noreply.github.com>
2022-10-15 10:41:05 +0530

    windows(chocolatey): update choco install script

--- windows/chocolatey.ps1
+++ windows/chocolatey.ps1
@@ -130,7 +130,7 @@ Function Install::Package($1) {
 Function Install::Packages() {
   Print::Bastion "Installing required system packages..."
 
-  Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+  Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
   If (-Not ($?)) {
     Print::Error "Unable to download and install Chocolatey."
   }
