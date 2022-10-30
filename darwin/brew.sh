7b6ddd3c6cffe553727b76c1fc6c03b414a47dc1
TRACTION <19631364+iamtraction@users.noreply.github.com>
2022-10-30 21:08:59 +0530

    darwin: don't install ffmpeg & youtube-dl
    Signed-off-by: TRACTION <19631364+iamtraction@users.noreply.github.com>

--- darwin/brew.sh
+++ darwin/brew.sh
@@ -170,9 +170,6 @@ function install::bastion() {
 function bastion::dependencies() {
   print::bastion "Installing Bastion dependencies..."
 
-  install::package "ffmpeg"
-  install::package "youtube-dl"
-
   cd "$BASTION_DIR"
   npm install --no-package-lock 1>/dev/null || \
     print::error "Unable to download and install node modules."
