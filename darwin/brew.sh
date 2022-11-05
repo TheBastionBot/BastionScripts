f64f6cee563ca1df0948bc28776c2629b74cf2bb
TRACTION <19631364+iamtraction@users.noreply.github.com>
2022-11-05 12:20:14 +0530

    darwin: upgrade node LTS version to 18
    Closes #21

--- darwin/brew.sh
+++ darwin/brew.sh
@@ -135,11 +135,11 @@ function install::nodejs() {
   print::bastion "Installing Node.js..."
 
   if ! hash node &>/dev/null; then
-    brew install "node@16" || \
+    brew install "node@18" || \
       print::error "Unable to download and install Node.js."
   fi
 
-  if [ "$(node --version | cut -d'v' -f 2 | cut -d'.' -f 1)" -ne 16 ]; then
+  if [ "$(node --version | cut -d'v' -f 2 | cut -d'.' -f 1)" -ne 18 ]; then
     print::error "Please upgrade Node.js LTS before running the installer again."
   fi
 
